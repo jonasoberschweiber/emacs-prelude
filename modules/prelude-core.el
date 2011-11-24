b71a5d9c4c6930f8220ddc1c3700bbdab45b6b19
jonasoberschweiber <jonas@frozenbeetle.net>
2011-11-24 13:05:35 +0100

    Personal changes

--- modules/prelude-core.el
+++ modules/prelude-core.el
@@ -327,7 +327,6 @@ there's a region, all lines that region covers will be duplicated."
   "Default coding hook, useful with any programming language."
   (flyspell-prog-mode)
   (prelude-local-comment-auto-fill)
-  (prelude-turn-on-whitespace)
   (prelude-turn-on-abbrev)
   (prelude-add-watchwords))
 
---
+++ personal/init.el
@@ -0,0 +1,7 @@
+;; Solarized theme
+(add-to-list 'load-path (concat (file-name-directory load-file-name) "emacs-color-theme-solarized"))
+
+(require 'solarized-dark-theme)
+
+(setq mac-command-modifier 'meta)
+(setq mac-option-modifier nil)
